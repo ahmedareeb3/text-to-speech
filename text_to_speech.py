3666c42053a442ab282a1d859c7a31f5ce366d7a
ahmedareeb3 <72149277+ahmedareeb3@users.noreply.github.com>
2023-02-24 21:12:46 +0530

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,35 @@
+
+
+# -------------------------------------------------------Method One--------------------------------------------------
+
+#Import Modules
+from gtts import gTTS
+import os
+
+myText=input("Enter the text\n")
+language ="en"
+
+#Converting text to audio
+speech = gTTS(text=myText, lang=language, slow=False)
+
+# Saving the converted audio in a mp3 file
+speech.save("audio.mp3")
+
+# Playing the converted file
+os.system("audio.mp3")
+# ----------------------------------------------------------End------------------------------------------------------
+
+
+
+
+# -------------------------------------------------------Method Two--------------------------------------------------
+
+#Import Modules
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say(myText)
+engine.runAndWait()
+# ----------------------------------------------------------End------------------------------------------------------
